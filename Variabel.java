540002f5677c991fb82fc767f986fffa532b6e8a
ranggashibyanurrijal <115127469+ranggashibyanurrijal@users.noreply.github.com>
2022-11-24 11:32:38 +0700

    Create Variabel.java

---
+++ Variabel.java
@@ -0,0 +1,11 @@
+public class Main 
+{
+    public static void main(String[] args) {
+        String nama = "Muhammad Rangga Shibyanurrijal";
+        int umur = 2002;
+        
+        System.out.println("Assalamualaikum");
+        System.out.println("Nama saya : "+ nama);
+        System.out.println("Saya lahir ditahun :"+ umur);
+    }
+}
